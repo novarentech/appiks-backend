7bb160c641420892133a0ed4036d2f901e0b55fe
d-arsya <kamaluddin.arsyad17@gmail.com>
2026-05-20 10:50:00 +0700

    chore: Disable external cron

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('compress:thumbnail')->everyTenSeconds();
-Schedule::command('generate:archtype')->hourly();
+// Schedule::command('compress:thumbnail')->everyTenSeconds();
+// Schedule::command('generate:archtype')->hourly();
